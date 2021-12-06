7842475fff6a5677ac1815fb1a0172263bbaf1ec
Luke Walter <lwalter5667@gmail.com>
2021-12-05 20:35:01 -0500

    Added levels/win-loss conditions

--- sketch.js
+++ sketch.js
@@ -122,7 +122,7 @@ var canvasHeight = 640;//window.innerHeight - 0.1;
 //breakout close (core mechanics)
 //mouse to control the player, click to start
 
-var player, wallTop, wallBottom, wallLeft, wallRight;
+var player, wallTop, wallBottom, wallLeft, wallRight, life1, life2, life3;
 var bricks;
 var MAX_SPEED = 9;
 var WALL_THICKNESS = canvasWidth / 7;
@@ -132,16 +132,21 @@ var BRICK_MARGIN = 4;
 var BRICK_SPEED = 0.0;
 var ROWS = 9;
 var COLUMNS = 16;
+var health = 3;
+var score = 0;
+
 var projectiles = [];
 var normal_bricks;
 
 function preload() {
+  
   v1 = loadImage("images/vehicle1.png");
   v2 = loadImage("images/vehicle2.png");
   bgImg = loadImage("images/background.png");
   shot = loadImage("images/shot.png");
   wall_l = loadImage("images/side_wall.JPG");
   wall_r = loadImage("images/side_wall.JPG");
+  life = loadImage("images/life.png");
 
   red = loadImage("images/red_brick.png");
   blue = loadImage("images/blue_brick.png");
@@ -152,7 +157,12 @@ function preload() {
 
   purple = loadImage("images/tough_brick1.png");
   tough = loadImage("images/tough_brick2.png");
-  ind = loadImage("images/indestructible_brick.png");
+  
+  st5 = loadImage("images/steel_brick5.png");
+  st4 = loadImage("images/steel_brick4.png");
+  st3 = loadImage("images/steel_brick3.png");
+  st2 = loadImage("images/steel_brick2.png");
+  st1 = loadImage("images/steel_brick1.png");
 
 } // preload
 
@@ -179,27 +189,77 @@ function setup() {
   wallRight.immovable = true;
   wallRight.addImage(wall_r);
 
+  barrier = createSprite(canvasWidth / 2, canvasHeight * 0.7, canvasWidth, 10);
+  barrier.immovable = true;
+  barrier.visible = false;
+
+  life1 = createSprite(68, 50, 45, 25);
+  life1.addImage("life1", life);
+  life1.immovable = true;
+  
+  life2 = createSprite(68, 100, 45, 25);
+  life2.addImage("life1", life);
+  life2.immovable = true;
+  
+  life3 = createSprite(68, 150, 45, 25);
+  life3.addImage("life1", life);
+  life3.immovable = true;
+
   bricks = new Group();
 
   var offsetX = 0;
   var offsetY = 0;
 
   bricks = new Group();
-  makeBrickRow(0.95);
-  makeBrickRow(1.02);
+  startLevel();
 
 } // setup
 
 function draw() {
   
+  if (bricks.length == 0) {
+    
+    for (let i = 0; i < projectiles.length; i++) {
+      var p = projectiles[i];
+      p.remove();
+
+    } // for
+
+    startLevel();
+
+  } // if
   background(bgImg);
 
   getInput();
+  barrier.collide(bricks, () => { BRICK_SPEED = 0.0; });
   
   for (let i = 0; i < projectiles.length; i++) {
     
     var p = projectiles[i];
-    p.bounce(wallTop);
+    p.collide(wallTop, () => {
+      
+      p.remove();
+      ammo++;
+      
+      switch (health) {
+        case 3:
+          life3.remove();health--;
+          break;
+        case 2:
+          life2.remove();health--;
+          break;
+        case 1:
+          life1.remove();health--;
+          noLoop();
+          alert("Game Over!\nScore = " + score);
+          break;
+        default:
+          break;
+
+      } // switch
+
+    });
+
     p.bounce(wallBottom);
     p.bounce(wallLeft);
     p.bounce(wallRight);
@@ -210,7 +270,7 @@ function draw() {
     
     } // for
 
-    if(p.bounce(player)) {
+    if(p.bounce(player)) {console.log("colliding");
       var swing = (p.position.x-player.position.x)/3;
       p.setSpeed(MAX_SPEED, p.getDirection()+swing);
     
@@ -228,23 +288,26 @@ function draw() {
 
   } // for
 
-  if (bricks.length > 0) {
-    
-    if (bricks[bricks.length - 1].position.y <= canvasHeight * 0.95) {
-      makeBrickRow(1.02);
-
-    } else if (bricks[0].position.y <= canvasHeight * 0.1) {
-      BRICK_SPEED = 0;
-
-    } // if
-
-  } // if
-
 } // draw
 
-function makeBrickRow(factor) {
+function startLevel() {
   
-  var rowY = canvasHeight * factor;
+  barrier.position.y = canvasHeight * (0.5);
+
+  makeBrickRow(1.02);
+  makeBrickRow(1.09);
+  makeBrickRow(1.16);
+  makeBrickRow(1.23);
+  makeBrickRow(1.30);
+
+  BRICK_SPEED = 1.5; 
+
+} // level
+
+var difficulty_factor = 10;
+function makeBrickRow(location_factor) {
+  
+  var rowY = canvasHeight * location_factor;
   var rowLength = 7;
 
   for(var r = 0; r < rowLength; r++) {
@@ -253,34 +316,48 @@ function makeBrickRow(factor) {
     var brick = createSprite(canvasWidth / 2 + offset, rowY, BRICK_W, BRICK_H);
     brick.immovable = true;
 
-    var type = Math.trunc(Math.random() * 4);
+    var type = Math.trunc(Math.random() * difficulty_factor);
 
     switch (type) {
       
       case 0:
+      case 1:
+      case 2:
         brick.remove();
         break;
       
-      case 1:
-
+      case 3:
+      case 4:
+      case 5:
+      case 6:
         var variant = Math.trunc(Math.random() * 4);
         brick.addAnimation("1", normal_bricks[variant ]);
         brick.changeAnimation("1");
+        bricks.add(brick);
         break;
       
-      case 2:
+      case 7:  
+      case 8:
+      case 11:
+      case 13:
+      case 15:  
         brick.addAnimation("2", tough);
         brick.addAnimation("1", purple);
         brick.changeAnimation("2");
+        bricks.add(brick);
         break;
       
-      case 3: 
-        brick.addAnimation("5", ind);
-        brick.addAnimation("4", ind);
-        brick.addAnimation("3", ind);
-        brick.addAnimation("2", ind);
-        brick.addAnimation("1", red);
+      case 9: 
+      case 10:
+      case 12:
+      case 14:
+        brick.addAnimation("5", st5);
+        brick.addAnimation("4", st4);
+        brick.addAnimation("3", st3);
+        brick.addAnimation("2", st2);
+        brick.addAnimation("1", st1);
         brick.changeAnimation("5");
+        bricks.add(brick);
         break;
       
       default:
@@ -288,12 +365,8 @@ function makeBrickRow(factor) {
         break;
 
     } // switch
-
-    bricks.add(brick);
   
   } // for
-
-  
 
 } // makeBrickRow
 
@@ -373,7 +446,8 @@ function brickHit(ball, brick) {
 
   } else {
     brick.remove();
-
+    score += 100;
+     
   } // if
   
 } // brickHit
